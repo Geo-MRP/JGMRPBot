23930a55bb8e63098a005b67e54980091c55ef89
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-05-30 20:05:59 +0200

    feat(core): add initial command and base MVC structure

--- src/com/GMRP/Main.java
+++ src/com/GMRP/Main.java
@@ -1,10 +1,57 @@
 package com.GMRP;
 
+import java.io.IOException;
+import java.util.*;
+
+import com.GMRP.core.gitManager.GitManager;
+import com.GMRP.features.SlashCommandController;
+import com.GMRP.features.aboutCommand.AboutEmbedView;
+import com.GMRP.views.shared.BotEmbedBuilder;
+import com.GMRP.features.aboutCommand.AboutCommandController;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Initialize Models
+		// GMRP Repo
+		BotConfig.init();
+		try {
+			GitManager GMRPRepoManager = new GitManager(".");
 
+			// Slash Commands
+			List<SlashCommandController> slashCommands = new ArrayList<>();
+
+			AboutEmbedView aboutEmbedView = new AboutEmbedView();
+			slashCommands.add(new AboutCommandController(aboutEmbedView, GMRPRepoManager));
+
+			String myToken = BotConfig.getInstance().getToken();
+			JDABuilder builder = JDABuilder.createDefault(myToken);
+
+			for (SlashCommandController controller : slashCommands) {
+				builder.addEventListeners(controller); // Tell JDA to listen to them
+			}
+
+			JDA jda = builder.build().awaitReady();
+
+			// set bot of BotEmbedBuilder
+			BotEmbedBuilder.setAvatarUrl(jda.getSelfUser().getAvatarUrl());
+
+			// Batch Register the Slash Commands
+			List<SlashCommandData> commandSetups = new ArrayList<>();
+			for (SlashCommandController controller : slashCommands) {
+				commandSetups.add(controller.getCommandSetup());
+			}
+
+			// Push the list of command setups to Discord
+			jda.updateCommands().addCommands(commandSetups).queue();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
