e5add37cab64c8c4120f8b038281c41dd8a51f19
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-08-11 23:01:02 +0200

    feat: implement dynamic version retrieval using `version.properties`

--- src/main/java/com/GMRP/features/aboutCommand/AboutCommandController.java
+++ src/main/java/com/GMRP/features/aboutCommand/AboutCommandController.java
@@ -11,8 +11,8 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 public class AboutCommandController extends ListenerAdapter implements SlashCommandController {
-	private AboutEmbedView view;
-	private GitManager gitManager;
+	private final AboutEmbedView view;
+	private final GitManager gitManager;
 
 	// Constructor necessary due to having a View and/or Model.
 	// We don't want to create a new instance of a View or Model each time the
@@ -34,7 +34,7 @@ public class AboutCommandController extends ListenerAdapter implements SlashComm
 		if (event.getFullCommandName().equalsIgnoreCase("about")) {
 			String ownerMention = "<@" + BotConfig.getInstance().getOwnerId() + ">";
 			String currentBranch = gitManager.getCurrentBranch();
-			String version = "V" + gitManager.countFeatureCommits() + "." + gitManager.countAllCommits();
+			String version = VersionReader.getVersion();
 			MessageEmbed aboutEmbed = view.formatAboutEmbed(ownerMention, currentBranch, version);
 			event.replyEmbeds(aboutEmbed).queue();
 		}
