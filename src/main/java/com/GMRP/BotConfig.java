66e0ffed935f8eab6ba2fd2ccf8d0204ea1ab1c0
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-08-15 15:36:24 +0200

    style: add empty lines for improved readability and configure Checkstyle for unused and redundant imports

--- src/main/java/com/GMRP/BotConfig.java
+++ src/main/java/com/GMRP/BotConfig.java
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
+
 package com.GMRP;
 
 import org.json.JSONObject;
--- src/main/java/com/GMRP/views/shared/BotEmbedBuilder.java
+++ src/main/java/com/GMRP/views/shared/BotEmbedBuilder.java
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
+
 package com.GMRP.views.shared;
 
 import net.dv8tion.jda.api.EmbedBuilder;
